6160aaf7db72706f722ca13e630a4edd0a99ae02
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2025-06-01 14:34:54 +0200

    Trying to resolve merge conflict
    Moving falsly committed commit to the correct branch, branching from dev
    instead of main.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './home/Home.tsx'
 import Layout from './Layout.tsx'
-import SomePage from './somePage/SomePage.tsx'
+import Modules from './modules/Modules.tsx'
+import Studyplan from './studyplan/Studyplan.tsx'
+import Todo from './todo/Todo.tsx'
+//import React from 'react'
+//import ReactDOM from 'react-dom';
 
 function App() {
 
   return (
-    <div>
-      <Layout>{SomePage()}</Layout>
-    </div>
+    <Layout>
+        <Routes>
+          <Route path="/" element={Home()} />
+          <Route path="/studyplan" element={Studyplan()} />
+          <Route path="/todo" element={Todo()} />
+          <Route path="/modules" element={Modules()} />
+        </Routes>
+    </Layout>
   )
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,14 @@
+import NavbarButton from "./navbarButton/NavbarButton";
+import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <NavbarButton to="/" text="Home" icon="house-solid.svg"/>
+      <NavbarButton to="/studyplan" text="Lernplan" icon="graduation-cap-solid.svg"/>
+      <NavbarButton to="/modules" text="Module" icon="book-open-reader-solid.svg"/>
+      <NavbarButton to="/todo" text="ToDo" icon="circle-check-regular.svg"/>
+    </div>
   )
 }
 
