7ca08be48de7e2cb5b94dd1383d373fa63b114c6
Nico N. (KA) <nileno@web.de>
2025-06-03 22:49:45 +0200

    Create own ExamplePage

---
+++ src/app/gridExamplePage/gridExamplePage.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Grid } from "../../components/grid/Grid";
+import type { BoxData } from "../../components/grid/Box";
+
+function GridExamplePage() {
+  const newElement: BoxData = { id: "5", title: "Box 3" };
+
+  const [items, setItems] = useState<BoxData[]>([
+    { id: "1", title: "Box 1" },
+    { id: "2", title: "Ein sehr sehr langer Titel" },
+  ]);
+
+  const addItem = () => {
+    setItems(oldItems => [...oldItems, newElement]);
+  }
+
+  const handleRename = (id: string, newTitle: string) => {
+    console.log(`Rename: ID=${id}, Neuer Titel=${newTitle}`);
+  };
+
+  const handleDelete = (id: string) => {
+    console.log(`Delete: ID=${id}`);
+  };
+
+  const handleClick = (id: string) => {
+    console.log(`Clicked: ID=${id}`);
+  };
+
+  return (
+    <div className="app">
+      <button onClick={addItem}>Add Item</button>
+      <Grid
+        items={items}
+        setItems={setItems}
+        onRename={handleRename}
+        onDelete={handleDelete}
+        onClick={handleClick}
+      />
+    </div>
+  );
+}
+
+export default GridExamplePage;
--- src/app/somePage/SomePage.tsx
+++ src/app/somePage/SomePage.tsx
@@ -1,48 +1,13 @@
-import { useState } from "react";
-import { Grid } from "../../components/grid/Grid";
-import type { BoxData } from "../../components/grid/Box";
+
+
+// just an example
 
 function SomePage() {
-  const elements1: BoxData[] = [
-    { id: "1", title: "Box 1" },
-    { id: "2", title: "Box 2" },
-  ];
-
-  const newEleement: BoxData = { id: "5", title: "Box 3" };
-
-  const [items, setItems] = useState<BoxData[]>([
-    { id: "1", title: "Box 1" },
-    { id: "2", title: "Ein sehr sehr langer Titel" },
-  ]);
-
-  const addItem = () => {
-    setItems(oldItems => [...oldItems, newEleement]);
-  }
-
-  const handleRename = (id: string, newTitle: string) => {
-    console.log(`Rename: ID=${id}, Neuer Titel=${newTitle}`);
-  };
-
-  const handleDelete = (id: string) => {
-    console.log(`Delete: ID=${id}`);
-  };
-
-  const handleClick = (id: string) => {
-    console.log(`Clicked: ID=${id}`);
-  };
-
   return (
-    <div className="app">
-      <button onClick={addItem}>Add Item</button>
-      <Grid
-        items={items}
-        setItems={setItems}
-        onRename={handleRename}
-        onDelete={handleDelete}
-        onClick={handleClick}
-      />
-    </div>
-  );
+    <p>
+      Some main Page
+    </p>
+  )
 }
 
 export default SomePage;
