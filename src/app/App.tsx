e5b863e154f0ffbafa204921a9cc0538852967a4
Nico Noeller <nico.noeller@sap.com>
2025-06-04 22:05:02 +0200

    Fixed the bug with the contextmenu button and align it better

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import '../colors.css'
 
 import Layout from './Layout.tsx'
 import PopUpExample from './popUpExamplePage/PopUpExample.tsx';
-import GridExamplePage from './gridExamplePage/gridExamplePage.tsx';
+import GridExamplePage from './gridExamplePage/GridExamplePage.tsx';
 
 const App: React.FC = () => {
   return (
