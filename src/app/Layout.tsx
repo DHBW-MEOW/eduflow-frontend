b081ae2e7140fe37329bd65d6d46b48d1a142429
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2025-06-08 03:01:26 +0200

    Created states with set functions for the header attributes to be able to change them dynamically later

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -2,14 +2,20 @@ import Header from "../components/header/Header.tsx"
 import Navbar from "../components/navbar/Navbar.tsx"
 import "../styles.css"
 
+import {useState} from 'react'
+
 function Layout({
   children
 }: {
   children: React.ReactElement
 }) {
+  const [rightButtonState, setRightButtonState] = useState({on: true, text:"", icon:"user-regular.svg", link:"/profile"});
+  const [leftButtonState, setLeftButtonState] = useState({on: true, text: "", icon: "house-solid.svg", link: "/"})
+  const [textState, setTextState] = useState("Hello User!")
+
   return (
     <div className="layout">
-      <Header button={{text: "", icon: "house-solid.svg", to: "/"}} text="Hello User!"/>
+      <Header leftButton={leftButtonState} text={textState} rightButton={rightButtonState}/>
       <div className="page">{children}</div>
       <Navbar/>
     </div>
