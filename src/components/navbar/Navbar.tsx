e209f659b2cfb1764f247d3b633ece5e538cc4e0
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2025-06-09 02:36:09 +0200

    Fixed argument name of LinkButton inside of Navbar.tsx file

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,10 +4,10 @@ import "./Navbar.css"
 function Navbar() {
   return (
     <div className="navbar">
-      <LinkButton to="/" text="Home" icon="house-solid.svg"/>
-      <LinkButton to="/studyplan" text="Lernplan" icon="graduation-cap-solid.svg"/>
-      <LinkButton to="/modules" text="Module" icon="book-open-reader-solid.svg"/>
-      <LinkButton to="/todo" text="ToDo" icon="circle-check-regular.svg"/>
+      <LinkButton link="/" text="Home" icon="house-solid.svg"/>
+      <LinkButton link="/studyplan" text="Lernplan" icon="graduation-cap-solid.svg"/>
+      <LinkButton link="/modules" text="Module" icon="book-open-reader-solid.svg"/>
+      <LinkButton link="/todo" text="ToDo" icon="circle-check-regular.svg"/>
     </div>
   )
 }
