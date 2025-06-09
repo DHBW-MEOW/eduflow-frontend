38873387db26a17619fd887b168ee80262eaa093
Nico N. (PC) <nileno@web.de>
2025-06-09 11:24:29 +0200

    - merged Grid - create basic Modul- and TopicPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import '../colors.css'
 import Layout from './Layout.tsx'
+import ModulPage from './modulPage/ModulPage.tsx'
+import TopicPage from './modulPage/TopicPage.tsx'
 
 const App: React.FC = () => {
   return (
     <Layout>
-      <h1>aöslkdjfaösldjf</h1>
+        <Routes>
+          <Route path="/" element={ModulPage()} />
+          <Route path="/module/:id" element={TopicPage()} />
+        </Routes>
     </Layout>
   )
 }
