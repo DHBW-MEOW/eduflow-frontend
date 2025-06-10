397483f65c865ab3106783fc3cb12e56ad67eed1
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2025-06-11 00:20:39 +0200

    Changed home button link to /home instead of root directory

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import "./Navbar.css"
 function Navbar() {
   return (
     <div className="navbar">
-      <LinkButton link="/" text="Home" icon="house-solid.svg"/>
+      <LinkButton link="/home" text="Home" icon="house-solid.svg"/>
       <LinkButton link="/studyplan" text="Lernplan" icon="graduation-cap-solid.svg"/>
       <LinkButton link="/modules" text="Module" icon="book-open-reader-solid.svg"/>
       <LinkButton link="/todo" text="ToDo" icon="circle-check-regular.svg"/>
