29f06d72758b1b3268a8e6eed00d3ddf1e781424
Nico N. (KA) <nileno@web.de>
2025-06-10 22:41:36 +0200

    Compatibility with Routing-Framework - changed path - delete some unused imports in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,6 @@
-import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import '../colors.css'
 import Layout from './Layout.tsx'
-import Home from './home/Home.tsx'
-import Modules from './modules/Modules.tsx'
-import Studyplan from './studyplan/Studyplan.tsx'
-import Todo from './todo/Todo.tsx'
-import SomePage from './somePage/SomePage.tsx'
-import { Children } from 'react'
 
 const App: React.FC = () => {
   return (
