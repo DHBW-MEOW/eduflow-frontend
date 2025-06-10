3fce641da3a3353e8d72a92f8c824c431676f6d9
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2025-06-11 00:00:12 +0200

    Added example pages to test new routing

---
+++ src/app/login/Login.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Login() {
+  return (
+    <div>
+      <div>Hello this is the Login page</div>
+    </div>
+  )
+}
---
+++ src/app/register/Register.tsx
@@ -0,0 +1,8 @@
+
+export default function Register() {
+  return (
+    <div>
+      <div>Hello this is the Register page</div>
+    </div>
+  )
+}
