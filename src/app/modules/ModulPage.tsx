030f449c2454f788ca852c7e24feb2a290702cad
Nico N. (KA) <nileno@web.de>
2025-06-10 23:14:32 +0200

    Synchronization delete/rename function with backend

--- src/app/modules/ModulPage.tsx
+++ src/app/modules/ModulPage.tsx
@@ -12,12 +12,44 @@ function ModulPage() {
         setItems(oldItems => [...oldItems, newElement]);
     };
 
-    const handleRename = (id: number, newTitle: string) => {
-        console.log(`Rename: ID=${id}, Neuer Titel=${newTitle}`);
+    const handleRename = async (id: number, newTitle: string) => {
+        try {
+            await fetchFromBackend<void>({
+                method: "POST",
+                endpoint: "data/course",
+                body: {
+                  id: id,
+                  name: newTitle,
+                },
+            });
+            console.log(`Rename: ID=${id}, Neuer Titel=${newTitle}`);
+        } catch (err) {
+            console.error("Fehler beim Umbenennen:", err);
+        }
     };
 
-    const handleDelete = (id: number) => {
-        console.log(`Delete: ID=${id}`);
+    const handleDelete = async (id: number) => {
+        try {
+            const topics = await fetchFromBackend<{ id: number }[]>({
+                method: "GET",
+                endpoint: `data/topic?course_id=${id}`,
+            });
+            for (const topic of topics) {
+                await fetchFromBackend<void>({
+                    method: "DELETE",
+                    endpoint: "data/topic",
+                    body: { id: topic.id },
+                });
+            };
+            await fetchFromBackend<void>({
+                method: "DELETE",
+                endpoint: "data/course",
+                body: { id: id },
+            });
+            console.log(`Delete: ID=${id}`);
+        } catch (err) {
+            console.error("Fehler beim Löschen:", err);
+        }
     };  
 
     const handleClick = (id: number) => {
@@ -25,13 +57,29 @@ function ModulPage() {
         console.log(`Clicked: ID=${id}`);
     };
 
-    useEffect(() => {
+    //TODO: zuerst habe ich mit .then().catch() gemacht, allerdings ist async/await besser -> schauen, ob alles noch gleich funktioniert
+    /*useEffect(() => {
         fetchFromBackend<{ id: number; name: string; details: string }[]>({
             method: "GET",
             endpoint: "data/course"
         })
         .then((data) => setItems(data))
         .catch((err) => console.error(err));
+    }, []);*/
+
+    useEffect(() => {
+        const loadData = async () => {
+            try {
+                const data = await fetchFromBackend<{ id: number; name: string; details: string }[]>({
+                    method: "GET",
+                    endpoint: "data/course",
+                });
+                setItems(data);
+            } catch (err) {
+                console.error("Fehler beim Laden der Kurse:", err);
+            }
+        };    
+      loadData();
     }, []);
 
     return (
