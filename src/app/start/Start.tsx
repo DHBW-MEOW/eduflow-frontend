635fd16af635da3258b073b489d1a25c42d84951
Nico Noeller <nico.noeller@sap.com>
2025-06-17 15:15:52 +0200

    First structure

--- src/app/start/Start.tsx
+++ src/app/start/Start.tsx
@@ -1,8 +1,12 @@
+import './Start.css'
+import '../colors.css'
 
 export default function Start() {
   return (
     <div>
-      <div>Hello this is the start page</div>
+      <div className="parent-container"></div>
+      <h2>Willkommen bei EduFlow!</h2>
+      <p>Das ist dein neuer Organisator für dein Lernerfolg. Leg jetzt los!</p>
     </div>
   )
 }
