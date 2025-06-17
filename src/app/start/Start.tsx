3d263f209ca59af88d69448734ebce59d93aacb3
Nico N. (KA) <nileno@web.de>
2025-06-17 22:21:15 +0200

    add Design and Buttons

--- src/app/start/Start.tsx
+++ src/app/start/Start.tsx
@@ -1,12 +1,29 @@
 import './Start.css'
-import '../colors.css'
+import '../../colors.css'
+import { useNavigate } from "react-router-dom";
+import OptionButton from '../../components/optionButtons/OptionButton'
 
 export default function Start() {
+  const navigate = useNavigate()
+  const handleLogin=() => {
+    navigate("/login")
+  }
+
+  const handleRegister=() => {
+    navigate("/register")
+  }
+
   return (
     <div>
-      <div className="parent-container"></div>
-      <h2>Willkommen bei EduFlow!</h2>
-      <p>Das ist dein neuer Organisator für dein Lernerfolg. Leg jetzt los!</p>
+      <div className="parent-container">
+        <h2 className="heading">Willkommen bei EduFlow!</h2>
+        <p className="text">Das ist dein neuer Organisator für dein Lernerfolg. Leg jetzt los!</p>
+
+        <div className="button-container">
+          <OptionButton label={"Anmelden"} onClick={handleLogin}></OptionButton>
+          <OptionButton label={"Registrieren"} onClick={handleRegister}></OptionButton>
+        </div>
+      </div>
     </div>
   )
 }
