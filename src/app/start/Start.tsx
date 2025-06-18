e09c1f507a87ecfba49bb6b567f5d9ff59f6821a
Cynthia Oleksy <oleksy2701@gmail.com>
2025-06-18 11:42:47 +0200

    Add Roboter Image

--- src/app/start/Start.tsx
+++ src/app/start/Start.tsx
@@ -16,8 +16,17 @@ export default function Start() {
   return (
     <div className="center-container">
       <div className="parent-container">
-        <h2 className="heading">Willkommen bei EduFlow! <br/> Dein Weg zu organisiertem und erfolgreichem Lernen beginnt hier.</h2>
-        <p className="text">Dein smarter Begleiter, der dir hilft, dein volles Lernpotenzial zu entfalten. Organisiere deine Lernreise spielend leicht und erreiche deine Ziele mit Freude. Bist du bereit, loszulegen?</p>
+        <h2 className="heading">
+          Willkommen bei EduFlow! <br/> Dein Weg zu organisiertem und erfolgreichem Lernen beginnt hier.
+        </h2>
+
+        <div className='div-image'>
+          <img src="/Roboter.png" alt="Bild eines Roboters, der Bücher in der Hand hält."/>
+        </div>
+
+        <p className="text">
+          Dein smarter Begleiter, der dir hilft, dein volles Lernpotenzial zu entfalten. Organisiere deine Lernreise spielend leicht und erreiche deine Ziele mit Freude. Bist du bereit, loszulegen?
+        </p>
 
         <div className="button-container">
           <OptionButton label={"Anmelden"} onClick={handleLogin}></OptionButton>
