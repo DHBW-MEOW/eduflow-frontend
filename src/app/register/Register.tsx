2678d8c6512f68aeeb5a1984cfc29472f34051e7
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2025-06-24 10:48:52 +0200

    Created basic logic for registering a new account

--- src/app/register/Register.tsx
+++ src/app/register/Register.tsx
@@ -2,14 +2,49 @@ import InputField from "../../components/popUpCreate/inputOptions/InputField";
 import OptionButton from "../../components/optionButtons/OptionButton";
 import "./Register.css";
 import { useState } from "react";
+import { fetchFromBackend } from "../../fetchBackend";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+  const navigate = useNavigate();
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isInvalid, setIsInvalid] = useState(false);
+  const [userTaken, setUserTaken] = useState(false);  
+  const [successfullyRegistered, setSuccessfullyRegistered] = useState(false);
 
-  const handleRegister = () => {
+  const handleNavigation = () =>{
+    console.log("Navigating to login page");
+    navigate("/login");
+  }
+
+  const handleRegister = async (username: string, password: string) => {
     console.log("Register button clicked");
+    console.log("Uesrname:", username, "Password:", password);
+
+    try {
+      const response = await fetchFromBackend({
+        method: "POST",
+        endpoint: "auth/register",
+        body: {
+          username: username,
+          password: password
+        }
+      })
+      console.log("Response from backend:", response);
+      console.log("Successfully registered");
+      setSuccessfullyRegistered(true);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        if(error.message.includes("409")){
+          console.error("Username already taken");
+          setUserTaken(true);
+        }
+      } else {
+        throw error
+      }
+    }
   };
 
   const validateData = (username: string, password: string): boolean => {
@@ -28,40 +63,57 @@ export default function Register() {
 
   return (
     <div className="registerPage">
-      <div>Hier können Sie sich registrieren.</div>
-      <form onSubmit={(e) => {
-        // Prevent default form submission reloading the page
-        e.preventDefault();
-        if (validateData(username, password)) { 
-          handleRegister();
-          setIsInvalid(false);
-        } else {
-          setIsInvalid(true);
+      <h2>Registrierung</h2>
+      { !successfullyRegistered &&
+        <form onSubmit={(e) => {
+          console.log("Form submitted");
+          // Prevent default form submission reloading the page
+          e.preventDefault();
+          if (validateData(username, password)) { 
+            handleRegister(username, password);
+            setIsInvalid(false);
+          } else {
+            setIsInvalid(true);
+          }
         }
+        }>
+          <InputField
+            label="Benutzername"
+            name="username"
+            value={username}
+            isInvalid={isInvalid}
+            errorMessage={isInvalid ? "Bitte geben Sie einen Benutzernamen ein." : ""}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <InputField
+            label="Kennwort"
+            name="password"
+            value={password}
+            isInvalid={isInvalid}
+            errorMessage={isInvalid ? "Bitte geben Sie ein Kennwort ein." : ""}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {
+            userTaken && <div className="error-message">Dieser Benutzername ist leider schon vergeben.</div>
+          }
+          <OptionButton
+            label="Registrieren"
+            buttonType="optionButton"
+            onClick={() => {} /*handleRegister(username, password) */}
+          />
+        </form>
       }
-      }>
-        <InputField
-          label="Benutzername"
-          name="username"
-          value={username}
-          isInvalid={isInvalid}
-          errorMessage={isInvalid ? "Bitte geben Sie einen Benutzernamen ein." : ""}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <InputField
-          label="Kennwort"
-          name="password"
-          value={password}
-          isInvalid={isInvalid}
-          errorMessage={isInvalid ? "Bitte geben Sie ein Kennwort ein." : ""}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <OptionButton
-          label="Registrieren"
-          buttonType="optionButton"
-          onClick={handleRegister}
-        />
-      </form>
+      {
+        successfullyRegistered && 
+        <div className="success-message">
+          <div> Sie haben sich erfolgreich registriert! Bitte melden Sie sich an. </div>
+          <OptionButton
+            label="Zum Login"
+            buttonType="optionButton"
+            onClick={handleNavigation}
+          />
+        </div>
+      }
     </div>
   )
 }
