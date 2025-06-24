7f229210c93a5fa3721121bd1630c4b2f76f62cc
Nico Noeller <nico.noeller@sap.com>
2025-06-24 12:46:35 +0200

    Create homepage - remove margin in the Quicklinks-Component

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,16 +1,50 @@
-import type { Route } from "../../../.react-router/types/src/app/home/+types/Home.tsx";
+import './Home.css'
+import { useCallback, useEffect, useState } from "react";
+import { fetchStudyGoalData } from "../../api/fetchStudyGoalData.tsx";
+import StudyGoals from "../../components/studyGoals/StudyGoals.tsx";
+import type { ItemData } from "../studyplan/types.tsx";
+import Quicklinks from '../../components/quicklinks/Quicklinks.tsx';
 
-export async function clientLoader(){
+export async function clientLoader() {
   return {
     text: "This is the new Homepage",
   };
 }
 
-export default function Home({ loaderData }: Route.ComponentProps) {
+export default function Home() {
+  const [items, setListItems] = useState<ItemData[]>([]);
+
+  const loadPageData = useCallback(async () => {
+    try {
+      const studyplanData = await fetchStudyGoalData();
+
+      if (studyplanData != undefined) {
+        setListItems(studyplanData.itemList);
+      }
+    } catch (err) {
+      console.error("Error while loading Data:", err);
+    }
+  }, []);
+
+  useEffect(() => {
+      loadPageData();
+  }, [loadPageData])
+
   return (
-    <div>
-      <div>öalksdjöaslkdfj</div>
-      <div>{loaderData.text}</div>
+    <div className="home-container">
+      <div className='home-left-items'>
+        <h2>Meine Lernziele</h2>
+        <div className="home-studygoals">
+          <StudyGoals items={items} />
+        </div>
+      </div>
+
+      <div className='home-right-items'>
+        <h2>Quicklinks</h2>
+        <div className='home-quicklinks'>
+          <Quicklinks></Quicklinks>
+        </div>
+      </div>
     </div>
   )
 }
