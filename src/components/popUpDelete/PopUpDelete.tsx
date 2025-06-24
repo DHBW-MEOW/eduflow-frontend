a120ed42b4f9700470e113934aec28162c6377c9
Cynthia Oleksy <oleksy2701@gmail.com>
2025-06-24 21:38:24 +0200

    Added cross to close PopUp

--- src/components/popUpDelete/PopUpDelete.tsx
+++ src/components/popUpDelete/PopUpDelete.tsx
@@ -17,7 +17,7 @@ const PopUpDelete: React.FC<PopUpProps> = ({isOpen, content, onClickCancel, onCl
         <div className="popup-overlay" onClick={onClickCancel}>
             <div className="popup-content" onClick={(e) => e.stopPropagation()}>
                 <button className="popup-close-button" onClick={onClickCancel}>
-                    &times; {/* TODO: Close button - Not sure if needed */}
+                    &times;
                 </button>
                 <div className='delete-popup-content'>{content}</div>
                 <div className="delete-popup-buttons">
