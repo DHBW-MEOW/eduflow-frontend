76895c983155503683a40fa1a588a97949564470
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2025-06-25 14:20:07 +0200

    Added some dynamic changing of the header text but having some issues with authentication navigation

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -1,37 +1,55 @@
-import { useNavigate, Outlet, createCookie } from "react-router-dom"
+import { useMatch, useLocation, useNavigate, Outlet, createCookie } from "react-router-dom"
 import Header from "../components/header/Header.tsx"
 import Navbar from "../components/navbar/Navbar.tsx"
 import HeaderContext from "../app/HeaderContext.tsx"
 import "../styles.css"
 
-import {useEffect, useState} from 'react'
+import {use, useEffect, useState} from 'react'
 
 import { useAuth } from "./AuthContext.tsx";
 
 
 function AppLayout() {
   const navigate = useNavigate();
-  const {isAuthenticated, checkAuthentication, isLoaded} = useAuth();
+  const { token, username, isAuthenticated, checkAuthentication, isLoaded} = useAuth();
   const [rightButtonState, setRightButtonState] = useState({on: true, text:"", icon:"user-regular.svg", link:"/profile"});
   const [leftButtonState, setLeftButtonState] = useState({on: true, text: "", icon: "Logo.svg", link: "/home"})
   const [textState, setTextState] = useState("Hello User!")
 
+  const location = useLocation();
+  //const match = useMatch();
+
   useEffect(() => {
     if(isLoaded){
-      console.log("Checking authentication in AppLayout");
+      console.log("Checking authentication in AppLayout, Token:", token);
       checkAuthentication();
       if(!isAuthenticated) {
-        console.log("User is not authenticated, navigating to login page");
+        console.log("User is not authenticated, navigating to login page, is loaded and is not authenticated");
         navigate("/");
       }
     }
-  },[isLoaded, navigate]);
+  },[isLoaded, isAuthenticated, navigate]);
   // useEffect(() => { / I think not needed?
   //   if(!isAuthenticated) {
   //     console.log("User is not authenticated, navigating to start page");
   //     navigate("/");
   //   } 
   // }, [isAuthenticated]);
+  useEffect(() => {
+    console.log("Location changed to: ", location.pathname);
+    if(location.pathname === "/home") {
+      setLeftButtonState({on: true, text: "", icon: "Logo.svg", link: "/home"});
+      setTextState("Hello " + username +  "!");
+    }
+    if(location.pathname === "/studyplan") {
+      setLeftButtonState({on: false, text: "", icon: "Logo.svg", link: "/home"});
+      setTextState("Lernplan");
+    }
+    if(location.pathname === "/modules") {
+      setLeftButtonState({on: false, text: "", icon: "Logo.svg", link: "/home"});
+      setTextState("Module");
+    }
+  }, [location.pathname, username]);
 
   return (
     <HeaderContext value={{setLeftButtonState, setTextState}}>
