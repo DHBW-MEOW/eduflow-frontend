3b9650d22724fb4ad00212d30aacadeb9bdfc647
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2025-06-27 00:10:07 +0200

    Fixed console error when logging out

--- src/api/logout.tsx
+++ src/api/logout.tsx
@@ -1,8 +1,9 @@
 import type { FetchFromBackendType } from "./createFetcher";
 
-export const logout = async (fetchFromBackend: FetchFromBackendType) => {
+export const logout = async (unsafeFetchFromBackend: FetchFromBackendType) => {
+    console.log("Logging out...");
     try {
-            await fetchFromBackend({
+            await unsafeFetchFromBackend({
                 method: "POST",
                 endpoint: "auth/logout",
                 body: {},
