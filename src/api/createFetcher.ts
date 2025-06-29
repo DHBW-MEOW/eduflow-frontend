86c09fa8acc08931bd471811cc6279c15f64e8ea
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2025-06-29 23:18:46 +0200

    Added isLoaded parameter to fetchFromBackend function

--- src/api/createFetcher.ts
+++ src/api/createFetcher.ts
@@ -16,7 +16,7 @@ async function fetchAPIURL(): Promise<string> {
 export type FetchFromBackendType = ReturnType<typeof createFetcher>["fetchFromBackend"];
 
 export const createFetcher = (token: string | null, navigate: NavigateFunction) => {
-    async function fetchFromBackend<T>({ method, endpoint, body }: FetchOptions): Promise<T> {
+    async function fetchFromBackend<T>({ method, endpoint, body }: FetchOptions, isLoaded: boolean): Promise<T> {
         const api = await fetchAPIURL();
         
         const response = await fetch(`${api}/${endpoint}`, {
